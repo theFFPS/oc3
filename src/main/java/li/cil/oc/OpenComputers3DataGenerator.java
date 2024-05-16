764a20864f64d4315ab1f1dd3db41cc6b9b62a56
TheFFPS <shmelev.lev@gmail.com>
2024-05-16 16:23:36 +0300

    Initialize

---
+++ src/main/java/li/cil/oc/OpenComputers3DataGenerator.java
@@ -0,0 +1,11 @@
+package li.cil.oc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OpenComputers3DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
